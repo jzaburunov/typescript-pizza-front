4cefe449d47b10cb827677e4fdae3157a5463da5
Juri Zaburunov <juri.zaburunov@gmail.com>
2020-07-22 17:59:17 +0300

    [wip]: Add sidebar layout

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,73 @@
-import React, { Component } from "react";
+import React, { Component, ReactElement } from "react";
 import ReactDOM from "react-dom";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
-import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap/dist/css/bootstrap.min.css";
 import { PizzaList } from "./components/PizzaList";
+import { Layout } from "antd";
 import { reducers } from "./reducers";
+import { BasicProps } from "antd/lib/layout/layout";
 
 const store = createStore(reducers, applyMiddleware(thunk));
+const { Content, Sider } = Layout;
 
-class App extends Component {
+interface AppStateInterface {
+  collapsed: boolean;
+}
+
+class App extends Component<BasicProps, AppStateInterface> {
+  constructor(props: BasicProps) {
+    super(props);
+    this.state = {
+      collapsed: false,
+    };
+  }
+
   render() {
+    // Add sidebar
+    const { collapsed } = this.state;
+    const { children } = this.props;
+
     return (
-      <div className="container">
-        <PizzaList />
-      </div>
+      // <div className="container">
+      //   <PizzaList />
+      // </div>
+      <Layout style={{ minHeight: "100vh" }}>
+        <Sider className="app__sider" theme="dark" collapsed={collapsed}>
+          <div
+            className={
+              collapsed
+                ? `Navigation__header Navigation__header--collapsed`
+                : `Navigation__header`
+            }
+          >
+            {/* TODO */}
+            {/* <img
+              height={collapsed ? 32 : 84}
+              src={collapsed ? logoSmall : logoBig}
+              alt=""
+            /> */}
+          </div>
+          {/* <SidebarMenu pathname={pathname} /> */}
+        </Sider>
+        <Layout>
+          {/* <AppHeader
+            collapsed={collapsed}
+            onCollapse={(): void => setCollapsed(!collapsed)}
+          /> */}
+          <Content className="app__content">{children}</Content>
+        </Layout>
+      </Layout>
     );
   }
 }
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <App>
+      <PizzaList />
+    </App>
   </Provider>,
   document.querySelector("#root")
 );
