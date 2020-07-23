81d50cc0db06ac03759e7321fcfe54ba261c5ad1
Juri Zaburunov <juri.zaburunov@gmail.com>
2020-07-23 14:23:55 +0300

    [wip]: Render pizzas as list in the cart

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,9 +1,39 @@
 import React from "react";
 import { connect } from "react-redux";
+import { StoreState } from "../reducers";
+import { CartPizza } from "../reducers/cart";
+import { Pizza } from "../actions";
 
-const _Cart: React.FC = (props) => {
-  // Render a list 
-  return <div>Soy caja</div>;
+interface CartInterface {
+  cart: CartPizza[];
+  cartPizzas: Map<string, Pizza | undefined>;
+}
+
+const _Cart: React.FC<CartInterface> = (props) => {
+  const { cartPizzas, cart } = props;
+  // Render a list
+  const pizzasInCart = cart.map((p) => (
+    <li key={p._id}>{cartPizzas.get(p._id)?.name}</li>
+  ));
+
+  return <ul>{pizzasInCart}</ul>;
 };
 
-export const Cart = connect()(_Cart);
+function mapStateToProps(state: StoreState) {
+  // TODO Use selector to select pizzas from store by _id
+  const { cart, pizzas } = state;
+
+  const cartPizzas = cart.reduce((res: Map<string, Pizza>, current) => {
+    const found = pizzas.find((i) => i._id === current._id);
+    if (found) {
+      res.set(current._id, found);
+    }
+    return res;
+  }, new Map<string, Pizza>());
+  return {
+    cart,
+    cartPizzas,
+  };
+}
+
+export const Cart = connect(mapStateToProps)(_Cart);
